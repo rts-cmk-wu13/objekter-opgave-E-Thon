aa440471945685c1e29cbfddb5250da88acc9431
Emilie Thon <emilie.m.thon@gmail.com>
2025-02-03 13:59:13 +0100

    Task begun - concentration gone

--- js/index.js
+++ js/index.js
@@ -1,4 +1,29 @@
-// slå dig løs her... 
+//! HERO 
+let header = document.querySelector(".header");
+
+hero.forEach(function(headerHero) {
+    const heroDiv = document.createElement("div");
+    heroDiv.textContent = headerHero.hero;
+    header.appendChild(heroDiv);
+
+})
+
+
+//! SERVICES
+
+
+//! FACILITIES
+
+
+
+//! SITES
+
+
+
+//! ADVANTAGES
+
+
+
 
 //eksempel på at udskrive alle overskrifter i services i konsollen:
 services.forEach(service => console.log(service.headline))
