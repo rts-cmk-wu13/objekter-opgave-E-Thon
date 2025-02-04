894bc765e015899e7e8da6f2bc5b490e77f972b7
Emilie Thon <emilie.m.thon@gmail.com>
2025-02-04 10:45:52 +0100

    hero elements added to HTML

--- js/index.js
+++ js/index.js
@@ -1,29 +1,54 @@
-//! HERO 
-let header = document.querySelector(".header");
+document.addEventListener("DOMContentLoaded", function () {
+    //! HERO 
+    let heroElm = document.querySelector(".hero");
 
-hero.forEach(function(headerHero) {
+    //? oprettelse af elementer til Hero:
+    // div til hero indhold:
     const heroDiv = document.createElement("div");
-    heroDiv.textContent = headerHero.hero;
-    header.appendChild(heroDiv);
+    heroDiv.classList.add(".hero");
+    // Billede:
+    const heroImg = document.createElement("img");
+    heroImg.src = heroDiv.image;
+    // Overskrift:
+    const heroHeadline = document.createElement("h1");
+    heroHeadline.textContent = hero.headline;
+    // Copy tekst (beskrivelse):
+    const heroCopy = document.createElement("p");
+    heroCopy.textContent = hero.copy;
+    // Ikon: 
+    const heroIcon = document.createElement("img");
+    heroIcon.src = hero.icon;
 
-})
+    //? Tilføjelse af elementer til HTML:
+    heroDiv.appendChild(heroImg);
+    heroDiv.appendChild(heroHeadline);
+    heroDiv.appendChild(heroCopy);
+    heroDiv.appendChild(heroIcon);
 
+    // hero div tilføjes til heroElm (stedet der er lavet til hero i HTML)
+    heroElm.appendChild(heroDiv);
+    
+    
+    //! SERVICES
+    
+    
+    //! FACILITIES
+    
+    
+    
+    //! SITES
+    
+    
+    
+    //! ADVANTAGES
+    
+    
+    
+    
+    // eksempel på at udskrive alle overskrifter i services i konsollen:
+    // services.forEach(service => console.log(service.headline))
+    
+    
+  }); // ends DOMContentLoaded
+  
 
-//! SERVICES
-
-
-//! FACILITIES
-
-
-
-//! SITES
-
-
-
-//! ADVANTAGES
-
-
-
-
-//eksempel på at udskrive alle overskrifter i services i konsollen:
-services.forEach(service => console.log(service.headline))
